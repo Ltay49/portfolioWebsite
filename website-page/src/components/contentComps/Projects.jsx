67975f44227d8a003576b7c8a87b0a3541f687e0
Lewis Taylor <lewistaylor@MacBook-Air.local>
2025-03-04 15:53:52 +0000

    basiclayout

--- website-page/src/components/contentComps/Projects.jsx
+++ website-page/src/components/contentComps/Projects.jsx
@@ -10,7 +10,9 @@ export default function Projects (){
                 <div className="clipHalf">
                 <video id="hoverVideo" src={ELVideo} autoPlay loop muted></video>
                 </div>
-                <div className="contentHalf"></div>
+                <div className="contentHalf">
+                    <p className="ELText"> EuroLingo - 'Europe Under One Roof', is an interactive language learning game aimed more specifically for children but contains a bonus level for more advanced learners. This was a collaborative process, our team consisted of many European speakers which ultimately inspired our MVP. Please feel free to check it out here by clicking one of the below links...</p>
+                </div>
             </section>
             <section className="project3"></section>
             <section className="project4"></section>
