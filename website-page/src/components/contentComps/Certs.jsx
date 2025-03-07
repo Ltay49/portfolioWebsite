f2cf8f797486fb177109d2e90503fd16d61cfba6
Lewis Taylor <lewistaylor@MacBook-Air.local>
2025-03-07 16:51:41 +0000

    basiclayout

--- website-page/src/components/contentComps/Certs.jsx
+++ website-page/src/components/contentComps/Certs.jsx
@@ -1,16 +1,19 @@
-import CERT from "../../assets/Lewis-Taylor-certificate.jpeg.jpg"
-export default function Certs(){
-
-
-    return(
-
-        <section  className="certsAndStacksContainer" id="certification">
-                <section className="cert">
-                    <section className="overlay"></section>
-                    <p className="certText">Certification of completetion for 'Software Development Bootcamp in JavaScript' from Northcoders</p>
-                    <img className="certPic" src={CERT}></img>
-                </section>
-                <section className="stacks"></section>
+import CERT from "../../assets/Lewis-Taylor-certificate.jpeg.jpg";
+export default function Certs() {
+  return (
+    <section className="certsAndStacksContainer" id="certification">
+      <section className="cert">
+        <p className="Certtag">
+          Certification of completetion for 'Software Development Bootcamp in
+          JavaScript' from Northcoders
+        </p>
+        <section className="Projbutton">
+          <button className="btnPdfStyle">view PDF</button>
         </section>
-    )
+        <section className="overlay"></section>
+        <img className="certPic" src={CERT}></img>
+      </section>
+      <section className="stacks"></section>
+    </section>
+  );
 }
