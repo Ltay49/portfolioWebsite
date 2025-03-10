436ae459343b76dbb91080b591e77e09f42ae87b
Lewis Taylor <lewistaylor@MacBook-Air.local>
2025-03-10 11:41:59 +0000

    basiclayout

--- website-page/src/components/contentComps/Projects.jsx
+++ website-page/src/components/contentComps/Projects.jsx
@@ -4,9 +4,11 @@ import HOWTOPLAY from "../../assets/BarclaysMen.jpeg"
 import GAMEPLAY from "../../assets/BarclaysMen 2.jpeg"
 import WIN from "../../assets/BarclaysMen 3.jpeg"
 import { useState } from "react";
+import LUA from "../../assets/LuaLua.png"
 
 export default function Projects() {
 
+const [activeCard, setActiveCard] = useState(null);
 const [projectPage, setProjectPage] = useState(false)
 
     const newPageHandler = () =>{
@@ -18,19 +20,40 @@ const [projectPage, setProjectPage] = useState(false)
     }
 
   return (
-    <div className="projects">
+    <div className="projects" id="projects">
       { projectPage ? (
-        <section className="projectPage">
+        <section className="projectPage" id="projects">
+            <img className="barcBack1" src={LUA}></img>
+            {/* <img className="barcBack" src={LUA}></img> */}
             <section className="projectText">
+                <p className="projpageBarc">Barclaysmen</p>
                 <p className="blurb">This project is a Daily Game, i was inspired by Wordle to produce my own version but with a twist or Cruyf turn if you like... instead of a 5 letter word the objective to guess which Barclaysmen of the day it is. The game has 100+ players in all steming from the Barclays Era of English top flight football with an abundance of iconic players to choose from.  </p>
             </section>
-            <section className="projectName">
-                <p></p>
-            </section>
             <section className="projectVideo">
-            <img className="howToPlayPP" src={HOWTOPLAY}></img><p className="gptag">game play</p>
-            <img className="gamePlayPP" src={WIN}></img><p className="gotag">game over</p>
-            <img className="winPP" src={GAMEPLAY}></img><p className="howtotag">how to play</p>
+            <img className="howToPlayPP" src={HOWTOPLAY}   style={{ zIndex: activeCard === 'howToPlay' ? 10 : 2 }}></img>
+            <img className="gamePlayPP" src={GAMEPLAY} style={{ zIndex: activeCard === 'gamePlay' ? 10 : 3 }}></img>
+            <img className="winPP" src={WIN} style={{ zIndex: activeCard === 'gameOver' ? 10 : 1 }}></img>
+            <section className="projectGamePlay"  src={WIN} style={{ zIndex: activeCard === 'gamePlay' ? 10 : 3 }}>
+            <button className="gptagTop" onClick={() => setActiveCard('gamePlay')}>game play</button>
+                <p>hjj</p>
+                <p>ghfhgfv</p>
+                <p>ggfgvh</p>
+                <button className="gptag" onClick={() => setActiveCard('gamePlay')}>game play</button>
+            </section>
+            <section className="projectHowToPlay" style={{ zIndex: activeCard === 'howToPlay' ? 10 : 2 }}>
+            <button className="howtotagTop" onClick={() => setActiveCard('howToPlay')}>how to play</button>
+                <p>hjj</p>
+                <p>ghfhgfv</p>
+                <p>ggfgvh</p>
+                <button className="howtotag" onClick={() => setActiveCard('howToPlay')}>how to play</button>
+            </section>
+            <section className="projectGameOver" style={{ zIndex: activeCard === 'gameOver' ? 10 : 1 }}>
+            <button className="gotagTop" onClick={() => setActiveCard('gameOver')}>game over</button>
+                <p>hjj</p>
+                <p>ghfhgfv</p>
+                <p>ggfgvh</p>
+                <button className="gotag" onClick={() => setActiveCard('gameOver')}>game over</button>
+            </section>
                 <section>
                     <button onClick={newPageHandler}>X</button>
                 </section>
