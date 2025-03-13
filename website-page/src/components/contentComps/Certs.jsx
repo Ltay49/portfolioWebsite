3b6eb7293a965e2b4179da181e855eec0f4115a6
Lewis Taylor <lewistaylor@MacBook-Air.local>
2025-03-13 17:08:16 +0000

    basiclayout

--- website-page/src/components/contentComps/Certs.jsx
+++ website-page/src/components/contentComps/Certs.jsx
@@ -1,8 +1,25 @@
 import CERT from "../../assets/Lewis-Taylor-certificate.jpeg.jpg";
+import PHASER from "../../assets/Phaser.png"
+import HTML from "../../assets/html.png"
+import CSS from "../../assets/css.png"
+import REACT from "../../assets/React.png"
+import ReactNative from "../../assets/ReactNative.png";
+import ExpoGo from "../../assets/ExpoGo.png";
+import Expo from "../../assets/Expo.png";
+import MongoDB from "../../assets/MongoDB.png";
+import Mongoose from "../../assets/mongoose.png";
+import SQL from "../../assets/sql.png"
+import PG from "../../assets/pg.png"
+import RENDER from "../../assets/Render.png"
+import Netlify from "../../assets/netlify.png";
+import Railway from "../../assets/Railway.png";
+import SB from "../../assets/supabase.png"
+
+
 export default function Certs() {
   return (
     <section className="certsAndStacksContainer" id="certification">
-      <section className="cert">
+      {/* <section className="cert">
         <p className="Certtag">
           Certification of completetion for 'Software Development Bootcamp in
           JavaScript' from Northcoders
@@ -12,8 +29,31 @@ export default function Certs() {
         </section>
         <section className="overlay"></section>
         <img className="certPic" src={CERT}></img>
-      </section>
-      <section className="stacks"></section>
+      </section> */}
+        <section className="front">
+        <p>Front-End</p>
+            <img className="Stack" src={PHASER}></img>
+            <img className="Stack" src={HTML}></img>
+            <img className="Stack" src={CSS}></img>
+            <img className="Stack" src={REACT}></img>
+            <img className="Stack" src={ReactNative}></img>
+            <img className="Stack" src={Expo}></img>
+            <img className="Stack" src={ExpoGo}></img>
+        </section>
+        <section className="back"><p>Back-End</p>
+            <img className="StackB" src={PG}></img>
+            <img className="StackB" src={MongoDB}></img>
+            <img className="StackB" src={SQL}></img>
+            <img className="StackB" src={Mongoose}></img>
+           </section>
+        <section className="other"><p>Hosting/Deployment</p>
+            <img className="StackB" src={SB}></img>
+            <img className="StackB" src={RENDER}></img>
+            <img className="StackB" src={Railway}></img>
+            <img className="StackB" src={Netlify}></img>
+        </section>
+        {/* <section className="host"></section>
+        <section className="methods"></section> */}
     </section>
   );
 }
